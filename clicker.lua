c9afac854b750f454bd6f08f0b0dd5a3fd41dc30
D4rka4k <67048583+D4rka4k@users.noreply.github.com>
2022-08-13 20:36:03 +0100

    Update clicker.lua

--- clicker.lua
+++ clicker.lua
@@ -1,80 +1,105 @@
-getgenv().autotap = false;
-getgenv().autorebirth = false;
-getgenv().autohatch = false;
+if game.PlaceId == 5490351219 then
+       
+    getgenv().autotap = false;
+    getgenv().autorebirth = false;
+    getgenv().autohatch = false;
+
+    local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+
+    local w = library:CreateWindow("A") -- Creates the window
+
+        local b = w:CreateFolder("B") -- Creates the folder(U will put here your buttons,etc)
+
+        b:Label("Clicker",{
+            TextSize = 25; -- Self Explaining
+            TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
+            BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
+            
+        }) 
+
+        b:Toggle("AutoClick",function(bool)
+            getgenv().autotap = bool
+            print("auto tap is  ", bool);
+            if bool then
+                dotap();
+            end
+        end)
+
+        b:Toggle("AutoRebirth",function(bool)
+            getgenv().autorebirth = bool
+            print("Rebirth  ", bool);
+            if bool then
+                Autorebirths(1000);
+            end
+        end)
 
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("PrisonLife", "Sentinel")
+        b:Toggle("hatch",function(bool)
+            getgenv().autohatch = bool
+            print("Rebirth  ", bool);
+            if bool then
+                buyegg('basic', 5);
+            end
+        end)
 
 
-local Tab = Window:NewTab("Farming")
-local Section = Tab:NewSection("Farming")
-Section:NewToggle("ToggleText", "ToggleInfo", function(state)
-end)
-
-local Tab = Window:NewTab("Pets")
-local Section = Tab:NewSection("Pets")
-
-
-local Tab = Window:NewTab("Teleports")
-local Section = Tab:NewSection("Teleports")
-
-
-
-
-function dotap()
-    spawn (function()
-        while autotap == true do
-            local args = {[1] = 1}   
-            game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.ClickService.Click:FireServer(unpack(args))
-            wait()
+        function dotap()
+            spawn (function()
+                while autotap == true do
+                    local args = {[1] = 1}   
+                    game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.ClickService.Click:FireServer(unpack(args))
+                    wait()
+                end
+            end)
         end
-    end)
-end
-dotap()
+        dotap()
 
-function Autorebirths(rebirthamount)
-spawn(function()
-    while autorebirth do 
-        local args = {[1] = rebirthamount}
-        game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RebirthService.BuyRebirths:FireServer(unpack(args))
-        wait()
+        function Autorebirths(rebirthamount)
+            spawn(function()
+                while autorebirth do 
+                    local args = {[1] = rebirthamount}
+                    game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RebirthService.BuyRebirths:FireServer(unpack(args))
+                    wait()
+                    end
+                end)
+            end
+            
+            Autorebirths(10)
+
+        b:DestroyGui()
+
+        function buyegg(eggtype, egglimit)
+            spawn(function()
+                if autohatch then
+                    for i=1, egglimit, 1 do
+                        game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.EggService.Purchase:FireServer(eggtype)
+                        wait()
+                    end
+                end
+            end)
         end
-    end)
-end
+        
+        buyegg("basic", 5);
 
-Autorebirths(10)
-
-
-
-function buyegg(eggtype)
-    spawn(function()
-        if autohatch then
-                game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.EggService.Purchase:FireServer(eggtype)
+        function getlocation()
+        local player = game.Players.LocalPlayer;
+        if player.Character then
+                return player.Character.HumanoidRootPart.Position;
         end
-    end)
-end
+            return false;
+    end
 
-buyegg("basic", 5);
-
-function getlocation()
-   local player = game.Players.LocalPlayer;
-   if player.Character then
-        return player.Character.HumanoidRootPart.Position;
-   end
-    return false;
-end
-
-function teleport(placeCFrame)
+    function teleport(placeCFrame)
     local player = game.Players.LocalPlayer;
-    if player.Character then
-        player.Character.HumanoidRootPart.CFrame = placeCFrame;
-    end
- end
+        if player.Character then
+            player.Character.HumanoidRootPart.CFrame = placeCFrame;
+        end
+        end
 
-function teleportWorld(world)
-    if game:GetService("Workspace").Worlds:FindFirstChild(world) then
-        teleport(game:GetService("Workspace").Worlds[world].Teleport.CFrame)
+        function teleportWorld(world)
+            if game:GetService("Workspace").Worlds:FindFirstChild(world) then
+                teleport(game:GetService("Workspace").Worlds[world].Teleport.CFrame)
+            end
+        end
+        teleportWorld("Desert")
     end
-end
-teleportWorld("Desert")
